3b3ebcc851a831e238f06a48ab7bc0f56e6565cb
Pravallika <pravallika@rapidinnovation.dev>
2023-06-28 16:59:57 +0530

    date format

--- src/modules/sponsor/index.tsx
+++ src/modules/sponsor/index.tsx
@@ -128,7 +128,7 @@ const Sponsor = () => {
       const month = String(date.getMonth() + 1).padStart(2, "0");
       const day = String(date.getDate()).padStart(2, "0");
 
-      const formattedDate = `${year}-${month}-${day}`;
+      const formattedDate = `${day}-${month}-${year}`;
       return formattedDate;
     } else {
       return "";
--- src/modules/subscription/index.tsx
+++ src/modules/subscription/index.tsx
@@ -309,7 +309,7 @@ const Subscription = () => {
     const year = date.getFullYear();
     const month = ("0" + (date.getMonth() + 1)).slice(-2);
     const day = ("0" + date.getDate()).slice(-2);
-    const formattedDate = `${day}/${month}/${year}`;
+    const formattedDate = `${day}-${month}-${year}`;
     return formattedDate;
   };
 
